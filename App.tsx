a320c6e49069e1c7c438ea09fea35b9faa992be1
zhihui.zhou <zhihui.zhou@anker.io>
2024-06-05 21:05:42 +0800

    feat: 规范路由

--- App.tsx
+++ App.tsx
@@ -4,9 +4,7 @@
  *
  * @format
  */
-
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,71 +15,21 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 import { NavigationContainer } from '@react-navigation/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import BookShelf from './src/pages/BookShelf/index';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({ children, title }: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import { routes } from './src/router/NavigationConfig';
 
 const Stack = createNativeStackNavigator();
-
+ÍÍ
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {routes.map((route) => (
+          <Stack.Screen key={route.name} name={route.name} component={route.component} />
+        ))}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/router/NavigationConfig.ts
@@ -0,0 +1,19 @@
+import { NavigationKeys } from "./NavigationKeys";
+import BookShelf from "../pages/BookShelf";
+
+// 定义路由名称及其参数类型
+export type RootStackParamList = {
+    BookShelf: {
+        page_id: string;
+    };
+}
+
+/**
+ * 路由配置
+ */
+export const routes: {
+    name: string,
+    component: ({ navigation, route }: any) => JSX.Element,
+}[] = [
+        { name: NavigationKeys.BookShelf, component: BookShelf },
+    ]
